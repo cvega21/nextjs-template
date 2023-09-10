b8c18acdf13605718c8f9094319f80bec9cb7721
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2023-09-10 04:21:58 -0700

    add custom theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,23 @@
 // import { useRouter } from 'next/router';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useChat, useCompletion } from 'ai/react';
 
 import { Meta } from '@/layouts/Meta';
 
 const Index = () => {
-  // const router = useRouter();
-  const [address, setAddress] = useState('');
   const [code, setCode] = useState('');
-  // const [loading, setLoading] = useState(false);
-  // const [error, setError] = useState(false);
-  // const [result, setResult] = useState(null);
+  // const { completion, input, handleInputChange, handleSubmit } = useCompletion({
+  //   api:'/api/analyzeCode'
+  // });
+
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    api: '/api/analyzeCode'
+  })
+
+  // useEffect(() => {
+    
+  // }, [completion])
+
 
   return (
     <div className="flex min-h-screen min-w-full flex-col items-center border border-black">
@@ -19,7 +27,7 @@ const Index = () => {
       />
       <div className="flex flex-col items-center">
         <h1 className="text-5xl font-bold">AI Smart Contract Auditor</h1>
-        <p>Enter your smart contract address or code to analyze</p>
+        {/* <p>Enter your smart contract address or code to analyze</p>
         <input
           placeholder="Enter your smart contract address"
           className="mt-8 w-full border"
@@ -28,24 +36,34 @@ const Index = () => {
             setAddress(e.target.value);
           }}
         />
-        <p>OR</p>
-        <textarea
-          placeholder="Type your smart contract code"
-          className="w-full border"
-          value={code}
-          onChange={(e) => {
-            setCode(e.target.value);
-          }}
-        />
-        <button
-          className="my-8 rounded-lg border bg-blue-500 p-3 font-bold text-white"
-          onClick={() => {
-            // console.log('analyzing!');
-          }}
-          type="button"
-        >
-          Analyze Code
-        </button>
+        <p>OR</p> */}
+        <form onSubmit={handleSubmit}>
+          <textarea
+            placeholder="Type your smart contract code"
+            className="w-full border"
+            value={input}
+            onChange={handleInputChange}
+          />
+          <button
+            className="my-8 rounded-lg border bg-blue-500 p-3 font-bold text-white"
+            onClick={() => {
+              // console.log('analyzing!');
+            }}
+            type='submit'
+          >
+            Analyze Code
+          </button>
+        </form>
+        
+        <p>
+          {messages.map((message, index) => {
+            return (
+              <div key={index}>
+                <p>{message.content}</p>
+              </div>
+            );
+          })}
+        </p>
       </div>
     </div>
   );
